8c17c4d897dba2382cdfd45343adb5ea9ecdceb5
Timafey Dzyubinski <tdzyubinski@elinext.com>
2024-08-05 00:17:58 +0300

    Core: Add base 'PDFTron' configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,19 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, ViewChild} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import WebViewer, {UI, WebViewerInstance} from "@pdftron/webviewer";
+import ImagesToPDF from '@coderosh/images-to-pdf'
+import {forkJoin} from "rxjs";
+
+
+export interface Document {
+  name: string;
+  pages: DocumentPage[]
+}
+
+export interface DocumentPage {
+  number: number;
+  imageUrl: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -10,4 +24,21 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'WebView';
+export class AppComponent implements AfterViewInit{
+  @ViewChild('viewer') viewerRef!: ElementRef;
+
+  wvInstance!: WebViewerInstance;
+
+  ngAfterViewInit(): void {
+    WebViewer({
+      path: '../assets/lib',
+      initialDoc: 'https://pdftron.s3.amazonaws.com/downloads/pl/webviewer-demo.pdf',
+      fullAPI: true,
+    }, this.viewerRef.nativeElement).then((instance: WebViewerInstance): void => {
+
+      this.wvInstance = instance;
+
+      const { documentViewer} = instance.Core;
+      instance.UI.setLayoutMode(instance.UI.LayoutMode.Single);
+
 }
